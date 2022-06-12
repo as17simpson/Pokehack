6acc94e62577549945f3573189c09ae98e334b65
Denzil Charleson <as17simpson@gmail.com>
2022-06-12 22:09:00 +0100

    garies pokeon all changed

--- text/OaksLab.asm
+++ text/OaksLab.asm
@@ -28,19 +28,19 @@ _OaksLabText39::
 _OaksLabCharmanderText::
 	text "So! You want the"
 	line "fire #MON,"
-	cont "CHARMANDER?"
+	cont "VULPIX?"
 	done
 
 _OaksLabSquirtleText::
 	text "So! You want the"
 	line "water #MON,"
-	cont "SQUIRTLE?"
+	cont "POLIWAG?"
 	done
 
 _OaksLabBulbasaurText::
 	text "So! You want the"
 	line "plant #MON,"
-	cont "BULBASAUR?"
+	cont "BELLSPROUT?"
 	done
 
 _OaksLabMonEnergeticText::
